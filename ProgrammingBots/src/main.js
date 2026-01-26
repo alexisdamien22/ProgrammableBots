d7ea37a0637a5e06c5549b3a2f86526cfffde585
Alexis DAMIEN <alexisdamien@icloud.com>
2026-01-26 17:41:08 +0100

    added the ship at origin

--- ProgrammingBots/src/main.js
+++ ProgrammingBots/src/main.js
@@ -4,6 +4,8 @@ import { loadAssets } from "./loader/loadAssets.js";
 import { updateChunks } from "./core/chunkLoader.js";
 import { drawWorld } from "./render/drawWorld.js";
 import { CHUNK_SIZE } from "./world/worldVars.js";
+import { Chunks } from "./world/chunks.js";
+import { addAssetToGrid } from "./core/grid.js";
 
 const canvas = document.getElementById("grid");
 const ctx = canvas.getContext("2d");
@@ -12,7 +14,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 const tileSize = 20;
-const SEED = 12345;
+const SEED = 65536;
 
 await loadAssets();
 
@@ -20,13 +22,17 @@ let needsRedraw = true;
 let lastChunkX = null;
 let lastChunkY = null;
 
+camera.updateWorldPosition(tileSize, canvas);
+updateChunks(camera, SEED, 5);
+const chunk = Chunks.get(0, 0);
+addAssetToGrid(chunk.grid, {
+    type: "spaceShuttle",
+    origin: { x: 0, y: 0 },
+    localOffset: { dx: 0, dy: 0 }
+}, 0, 0);
+
 function frame() {
     camera.updateWorldPosition(tileSize, canvas);
-    console.log(
-        "FPS OK | camera",
-        camera.worldX.toFixed(2),
-        camera.worldY.toFixed(2)
-    );
 
     const cx = Math.floor(camera.worldX / CHUNK_SIZE);
     const cy = Math.floor(camera.worldY / CHUNK_SIZE);
